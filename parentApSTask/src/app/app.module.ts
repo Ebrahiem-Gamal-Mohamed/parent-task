c2e238f1a3fa9d9ea8fdbfbb6c1ae0c4e4154165
Ibrahim Gamal <ibrahim.gamal@itblocks.local>
2019-01-09 19:06:17 +0200

    install angular project and add some modules

---
+++ parentApSTask/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Modules ...
+import { AppRoutingModule } from './app-routing.module';
+// Components ...
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+  AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
