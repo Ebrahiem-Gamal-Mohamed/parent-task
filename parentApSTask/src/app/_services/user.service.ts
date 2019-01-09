2b928b02dca4e9ae7321f48cc3ec0ab48fdb2686
E Gamal <e.gamal@isafe-land.com>
2019-01-09 23:20:07 +0200

    implement bread-crumb component, user service and users-list component

--- parentApSTask/src/app/_services/user.service.ts
+++ parentApSTask/src/app/_services/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import * as Globals from '../globals';
+import { User } from '../_models/userModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  page_number: number;
+
+  constructor(private http: HttpClient) {
+      this.page_number = 1;
+   }
+
+  getAll() {
+      return this.http.get<User[]>(`${Globals.apiUrl}/users`, { params: { page: JSON.stringify(this.page_number) } });
+  }
+
+  getById(id: number) {
+      return this.http.get(`${Globals.apiUrl}/users/${id}`);
+  }
+
+  add(user: User) {
+      return this.http.post(`${Globals.apiUrl}/users`, user);
+  }
+
+  update(user: User) {
+      return this.http.put(`${Globals.apiUrl}/users/${user.id}`, user);
+  }
+
+  delete(id: number) {
+      return this.http.delete(`${Globals.apiUrl}/users/${id}`);
+  }
+
 }
--- parentApSTask/src/app/users/users-list/users-list.component.ts
+++ parentApSTask/src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { UserService } from 'src/app/_services/user.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { User } from 'src/app/_models/userModel';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-users-list',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
+  usersList: User[] = [];
+  users = new BehaviorSubject([]);
 
-  constructor() { }
+  page_title: string;
+  page_link: string;
+  finished: boolean;
+
+  constructor(
+    private userService: UserService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
+    this.page_link = 'Dashboard';
+    this.page_title = 'users list';
+    this.finished = false;
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    if (this.finished) { return; }
+
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.usersList = users;
+      this.userService.page_number++;
+    });
+    this.finished = true;
+  }
+
+  onScroll() {
+    console.log('scrolled!!');
+    this.getAllUsers();
   }
 
 }
